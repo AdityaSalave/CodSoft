741a2904ab440b794d720b55e105096743b1d97e
Aditya Salave <121479431+AdityaSalave@users.noreply.github.com>
2024-01-17 11:27:04 +0530

    Add files via upload

---
+++ Task 2/UserBankAccount.java
@@ -0,0 +1,25 @@
+package com.aditya.task2;
+public class UserBankAccount {
+    private double balance;
+
+    public UserBankAccount(double initialBalance) {
+        balance = initialBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public boolean withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
